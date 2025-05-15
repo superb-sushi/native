2c9407c8c87cc0458eaf902f944190b9e00421aa
superb-sushi <caleb.ang.zx@gmail.com>
2025-05-15 09:03:52 +0800

    Add minor updates

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+        <Text style={styles.app}>Open</Text>
+        <StatusBar style="auto" />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffF',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  app: {
+    flex: 1,
+    color: 'gray',
+    fontWeight: 'bold',
   },
 });
