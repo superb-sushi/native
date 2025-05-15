907ebe4e96f684936b85886afdf968a1a2fd64b6
superb-sushi <caleb.ang.zx@gmail.com>
2025-05-15 23:00:11 +0800

    Add top navigation bar and banner

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, SafeAreaView, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Button, View, ScrollView, Image } from 'react-native';
+import { useFonts, Roboto_400Regular } from '@expo-google-fonts/roboto';
+import NavBar from "./components/NavBar.js";
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-        <Text style={styles.app}>Open</Text>
-        <StatusBar style="auto" />
+      <View style={styles.navbar}>
+        <View style={styles.navbartop}>
+          <Image style={styles.dragon} source={require("./assets/dragon.png")}/>
+          <Text style={styles.navbartopText}>A XXXXX Government Agency Website</Text>
+        </View>
+        <View style={styles.navbarbot}>
+          <View style={styles.navSection}>
+            <NavBar />
+          </View>
+        </View>
+      </View>
+      <ScrollView contentContainerStyle={styles.viewPage}>
+        <Text>ScrollView</Text>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -14,12 +37,57 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#ffF',
-    alignItems: 'center',
     justifyContent: 'center',
   },
-  app: {
-    flex: 1,
-    color: 'gray',
-    fontWeight: 'bold',
+  viewPage: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    minHeight: '100vh',
+    backgroundColor: "#252934",
+    zIndex: '1'
   },
+  navbartop: {
+    height: '34px',
+    backgroundColor: '#EEEEEE',
+    width: '100%',
+    alignItems: 'center',
+    flexDirection: "row",
+    paddingLeft: '15vw',
+  },
+  navbartopText: {
+    fontFamily: "Roboto_400Regular",
+    color: "#404040",
+    fontSize: '15px',
+    letterSpacing: '0.75px',
+    fontWeight: '100'
+  },
+  dragon: {
+    height: '20px',
+    width: '20px',
+    marginRight: '7px'
+  },
+  navbar: {
+    width: '100%',
+    zIndex: '2',
+    position: "absolute",
+    top: '0px',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  navbarbot: {
+    backgroundColor: 'white',
+    width: '100%',
+    height: '82px',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  navSection: {
+    flexDirection: "row",
+    width: '70vw',
+    height: '100%',
+    minWidth: '50vw',
+    alignItems: "center",
+    minWidth: "1300px"
+  }
 });
---
+++ components/SearchBar.js
@@ -0,0 +1,72 @@
+import { StyleSheet, View, Image, TextInput, Pressable } from 'react-native';
+import {useState} from "react";
+import { useFonts, Roboto_400Regular } from '@expo-google-fonts/roboto';
+
+const Search = () => {
+
+  const [searchVal, setSearchVal] = useState("");
+
+  const [isSBActive, setIsSBActive] = useState(false);
+
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+  });
+
+  const handleSearchValue = (val) => {
+    setSearchVal(val)
+    val == "" ? setIsSBActive(false) : setIsSBActive(true);
+  }
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <View style={styles.searchContainer}>
+      <Image source={isSBActive ? require("../assets/activeSearch.png") : require("../assets/search.png")} style={styles.searchIcon}/>
+      <View style={styles.searchInputContainer}>
+        <TextInput placeholder={"What are you looking for?"} value={searchVal} onChangeText={(val) => handleSearchValue(val)} style={styles.searchInput} onFocus={() => setIsSBActive(true)}/>
+        {searchVal !== "" ? <Pressable onPress={() => handleSearchValue("")}><Image source={require("../assets/clear.png")} style={styles.clearIcon}/></Pressable> :<View></View>}
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  searchContainer: {
+    marginLeft: "4%",
+    minWidth: "42%",
+    height: "80px",
+    alignItems: "center",
+    flexDirection: "row"
+  },
+  searchIcon: {
+    height: "24px",
+    width: "24px",
+    objectFit: "contain",
+    marginLeft: "10px"
+  },
+  searchInputContainer: {
+    width: "88%",
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: "18px"
+  },
+  searchInput: {
+    width: "100%",
+    fontFamily: "Roboto_400Regular",
+    color: "#888888",
+    height: "24px",
+    fontSize: "17px",
+    fontWeight: "100",
+    outlineStyle: "none"
+  },
+  clearIcon: {
+    marginLeft: "14px",
+    height: "13px",
+    width: "13px",
+    objectFit: "contain"
+  },
+});
+
+export default Search
